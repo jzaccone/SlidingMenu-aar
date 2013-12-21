757abca7a7d80550e70b6139ba6b331708d2e2a3
minakov <minakov@gmail.com>
2013-12-21 16:46:48 +0200

    Create clone.sh

---
+++ clone.sh
@@ -0,0 +1,7 @@
+wget --no-check-certificate https://codeload.github.com/minakov/aar-quickstart/zip/master -O aar-quickstart.zip
+unzip aar-quickstart.zip
+rm aar-quickstart.zip
+mv aar-quickstart-master aar-quickstart
+cd aar-quickstart
+chmod +x build-gh-pages.sh
+mv SCELETON.md README.md
